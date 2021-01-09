00495dbfe318d484808e0d6cb2e8c3371fc0062f
王冠宇 <>
2021-01-09 22:30:03 +0800

    feat: init project

---
+++ 01HelloWolrd/HelloImooc.java
@@ -0,0 +1,6 @@
+public class HelloImooc {
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+		System.out.println("我爱学java");
+	}
+}
---
+++ Spring/s01/src/main/java/com/imooc/spring/ioc/SpringApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.spring.ioc;
+
+import com.imooc.spring.ioc.entity.Apple;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringApplication {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        Apple sweetApple = context.getBean("sweetApple", Apple.class);
+        System.out.println(sweetApple.getTitle());
+    }
+}
